dd11d3fa6c49120fb0039fe300675cfd18315c98
Jaime Andrés Monsalve Ballesteros <70330970+andresmonsalve19@users.noreply.github.com>
2021-10-24 23:19:10 -0500

    Subiendo la parte 2 del taller 2 de Python a GitHub

---
+++ main.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+        
+    def cambiarColor(self, color):
+        colores = ["rojo", "verde", "amarillo", "negro", "blanco"]
+        if (color == colores[0] or color == colores[1] or color == colores[2] or color == colores[3] or color == colores[4]): 
+            self.color = color    
+            
+            
+class Auto:
+    cantidadCreados = 0
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.registro = registro
+        self.motor = motor
+        
+    def cantidadAsientos(self):
+        numero_asientos = 0
+        
+        for i in range(0, len(self.asientos)):
+            if isinstance(asientos[i], Asiento):
+                numero_asientos += 1
+                
+        return numero_asientos
+    
+    def verificarIntegridad(self):
+        cadena = ""
+        
+        for i in self.asientos:
+            if (self.registro != i.registro or self.registro != self.motor.registro or i.registro != self.motor.registro):
+                cadena = "Las piezas no son originales"
+            else:
+                cadena = "Auto original"
+    
+        return cadena
+    
+
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+        
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+        
+    def asignarTipo(self, tipo):
+        if (tipo == "electrico" or tipo == "gasolina"):
+            self.tipo = tipo
+
